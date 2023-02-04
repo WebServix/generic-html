1640323c5a9a6c1c04b1bc9dcc091468ca04e54a
WebServix <arnaud@webservix.fr>
2023-02-04 00:32:54 +0000

    Update from https://github.com/WebServix/generic-builder/commit/71042c0114331cb2c7eab5414d5435cfd09b5c23

--- en/generate/_payload.js
+++ en/generate/_payload.js
@@ -1,1 +1,1 @@
-export default {data:{},prerenderedAt:1675470355246}
+export default {data:{},prerenderedAt:1675470771378}
--- fr/generate/_payload.js
+++ fr/generate/_payload.js
@@ -1,1 +1,1 @@
-export default {data:{},prerenderedAt:1675470355330}
+export default {data:{},prerenderedAt:1675470771465}
